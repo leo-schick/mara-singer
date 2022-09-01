0c8ab795028ad3ef5b49e86de8daacf77a170998
Leo Schick <leonhard.schick@gmail.com>
2022-09-01 11:20:01 +0200

    use setup.cfg, add Makefile, clean .gitignore, add github actions CI

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-from setuptools import setup, find_packages
-import re
+from setuptools import setup
 
-def get_long_description():
-    with open('README.md') as f:
-        return re.sub('!\[(.*?)\]\(docs/(.*?)\)', r'![\1](https://github.com/hz-lschick/mara-singer/raw/master/docs/\2)', f.read())
-
-setup(
-    name='mara-singer',
-    version='0.7.5',
-
-    description='Singer implementation for mara',
-
-    long_description=get_long_description(),
-    long_description_content_type='text/markdown',
-
-    url = 'https://github.com/hz-lschick/mara-singer',
-
-    install_requires=[
-        'singer-python>=5.9.0',
-        'mara-app>=2.0.2',
-        'mara-db>=4.7.0',
-        'mara-page>=1.5.1',
-        'mara-pipelines>=3.0.0'],
-
-    extras_require={
-        'test': ['pytest', 'pytest_click', 'mara-app>=2.2.0'],
-    },
-
-    packages=find_packages(),
-
-    author='Mara contributors',
-    license='MIT',
-
-    entry_points={},
-    python_requires='>=3.6'
-)
+setup()
